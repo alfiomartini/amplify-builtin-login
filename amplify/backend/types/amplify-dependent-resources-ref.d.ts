b872c21aeff08a6f10f98de7f66321984915a7cf
Alfio Martini <alfio.martini@gmail.com>
2022-12-29 23:14:48 -0300

    add built-in authentication and sign-out

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifybuiltinlogin34741290": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import { Amplify } from "aws-amplify";
+import awsConfig from "./aws-exports";
+import { withAuthenticator, useAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(awsConfig);
 
 function App() {
+  const { signOut } = useAuthenticator();
   return (
     <div className="App">
       <h2>Hello Amplify</h2>
+      <button onClick={() => signOut()}>Sign Out</button>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
